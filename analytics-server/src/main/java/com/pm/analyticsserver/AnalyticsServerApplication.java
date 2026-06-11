2f85a8152ecaa72c3912c42f3fbeeac9a23348fa
Swapnaneel <swapnaneel.log.in.16@gmail.com>
2026-06-11 02:14:24 -0400

    Initializing analytics service

---
+++ analytics-server/src/main/java/com/pm/analyticsserver/AnalyticsServerApplication.java
@@ -0,0 +1,13 @@
+package com.pm.analyticsserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalyticsServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnalyticsServerApplication.class, args);
+    }
+
+}
